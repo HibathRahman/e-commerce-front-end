c637a0c64a05a399f5e3653d74287dc7addb0ea7
HibathRahman <147071016+HibathRahman@users.noreply.github.com>
2023-11-18 23:25:30 +0530

    Update userRoutes.jsx

--- src/routes/userRoutes.jsx
+++ src/routes/userRoutes.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import UnAuth from "../components/unAuth/unAuth";
 import { Navigate } from "react-router";
 
-const UserRoutes = ({ user, children }) => {
+const UserRoutes = ({ children }) => {
+  const user = JSON.parse(localStorage.getItem("userInfo"));
   return user && !user?.isAdmin ? (
     <>{children}</>
   ) : !user && !user?.isAdmin ? (
